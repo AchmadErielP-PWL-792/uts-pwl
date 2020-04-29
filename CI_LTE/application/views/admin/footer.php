8246279950a9b87f3a736c9e2c9bcef67b3574d2
AchmadErielP-PWL-792 <tugaskuliah024@gmail.com>
2020-04-29 18:01:24 +0700

    first commit

---
+++ CI_LTE/application/views/admin/footer.php
@@ -0,0 +1,5 @@
+  <footer class="main-footer">
+    <strong>Copyright &copy; 2020-2021 <a href="http://adminlte.io">AchmadErielP@181240000792</a></strong>
+    <div class="float-right d-none d-sm-inline-block">
+    </div>
+  </footer>
